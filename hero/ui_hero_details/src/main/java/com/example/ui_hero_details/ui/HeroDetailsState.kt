d86df34bbbe6f1b962d54e931eb6fee2b9646762
Danik <porvipop@mail.ru>
2021-12-24 08:17:23 +0300

    Add Queue class

--- hero/ui_hero_details/src/main/java/com/example/ui_hero_details/ui/HeroDetailsState.kt
+++ hero/ui_hero_details/src/main/java/com/example/ui_hero_details/ui/HeroDetailsState.kt
@@ -1,10 +1,13 @@
 package com.example.ui_hero_details.ui
 
 import com.example.core.domain.ProgressBarState
+import com.example.core.domain.Queue
+import com.example.core.domain.UIComponent
 import com.example.hero_domain.Hero
 
 data class HeroDetailsState(
     val progressBarState: ProgressBarState = ProgressBarState.Idle,
     val hero: Hero? = null,
+    val errorQueue: Queue<UIComponent> = Queue(mutableListOf())
 
     )
